e97260c32b74e49f25c1912cebdfc2c183e0faeb
siwiakmichal <siwiakmichal@gmail.com>
2017-12-11 03:33:21 +0100

    Update

--- src/main/java/com/domain/java/DecodeMorseCode.java
+++ src/main/java/com/domain/java/DecodeMorseCode.java
@@ -1,8 +1,23 @@
 package com.domain.java;
+
 import java.util.LinkedHashMap;
+
 import java.util.LinkedHashSet;
 import java.util.Map;
 import java.util.Set;
+
+/**
+ * Program coding the text in the form of Morse code and decoding string of dots
+ * and dashes to the text. Individual letters in both the explicit text and the
+ * Morse code are not separated by a white character.
+ * 
+ * 
+ * @author Michał Siwiak
+ * @version 1.0
+ *
+ * @param String text
+ * @return text or set
+ */
 
 public class DecodeMorseCode {
 
